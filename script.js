7ca96a212db8019a80b4b349ad7f0452936baa67
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-01-28 15:51:09 -0600

    adjusted some variables

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var apiKey = "3c7499a3342523b606ee230e450c28e8";
 
 
-var searchButton = $(".searchButton");
+var searchBtn = $(".searchButton");
 
 for (var i = 0; i < localStorage.length; i++) {
     var city = localStorage.getItem(i);
@@ -12,7 +12,7 @@ for (var i = 0; i < localStorage.length; i++) {
 }
 // gets search answer info from openweathermap site
 var countKeys = 0;
-searchButton.click(function () {
+searchBtn.click(function () {
 
     var searchAnswer = $(".searchInput").val();
 
@@ -46,14 +46,14 @@ searchButton.click(function () {
             currentName.append(response.name + " " + timeStamp.toLocaleDateString("en-US"));
             currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
             
-            var currentTemp = currentName.append("<p>");
+            var newTemp = currentName.append("<p>");
             
-            currentName.append(currentTemp);
-            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "°F" + "</p>");
+            currentName.append(newTemp);
+            newTemp.append("<p>" + "Temperature: " + response.main.temp + "°F" + "</p>");
            
-            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+            newTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
             
-            currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "Mph" + "</p>");
+            newTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "Mph" + "</p>");
 
             
             var urlLink = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
